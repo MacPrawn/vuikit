bd9681828bc310480cd43e1cad26c7bf66642dab
Jean Le Clerc <jean@nibnut.com>
2016-11-24 10:46:41 -0500

    Debug events

--- src/lib/Table/index.js
+++ src/lib/Table/index.js
@@ -111,9 +111,8 @@ export default {
       })
     }
     this.sortOrder[this.fields[0].name] = 'asc'
-    this.$on('_clickRow', (rowID, row) => {
-      console.log('_clickRow event received', rowID, row)
-      this.$emit('clickRow', rowID, row)
+    this.$on('clickRow', (rowID, row) => {
+      console.log('clickRow event received', rowID, row, this)
     })
   },
   computed: {
